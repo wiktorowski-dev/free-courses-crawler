98136036daa5ea6c39ab43f78a9e8e4e001cc2ae
Kebirs <dklecha99@gmail.com>
2021-02-25 11:49:21 +0100

    works, hardcoded

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import scrape_courses
 
-scrape = scrape_courses.scrape_pepper()
 
 
+# promo = scrape_courses.scrape_pepper(url)
+
+# szkolenia_kursy = scrape_courses.scrape_pepper_szkolenia_kursy()
+# udemy = scrape_courses.scrape_pepper_udemy()
 
 
+urls = {'szkolenia i kursy': 'https://www.pepper.pl/grupa/szkolenia-i-kursy?page={}',
+        'uslugi i subskrypcje': 'https://www.pepper.pl/grupa/uslugi-i-subskrypcje?page={}',
+        'udemy.com': 'https://www.pepper.pl/promocje/udemy.com?page={}'}
 
+
+courses = scrape_courses.scrape_pepper(urls['szkolenia i kursy'],
+                                       urls['uslugi i subskrypcje'],
+                                       urls['udemy.com'])
--- scrape_courses.py
+++ scrape_courses.py
@@ -1,9 +1,13 @@
 import re
 import requests
+import inspect
 from bs4 import BeautifulSoup as bs
 
 # url_pepper = 'https://www.pepper.pl/grupa/internet-i-uslugi?page={}'
-url_pepper = 'https://www.pepper.pl/grupa/uslugi-i-subskrypcje?page={}'
+# url_pepper = 'https://www.pepper.pl/grupa/uslugi-i-subskrypcje?page={}'
+# urls = {'szkolenia i kursy': 'https://www.pepper.pl/grupa/szkolenia-i-kursy?page={}',
+#         'uslugi i subskrypcje': 'https://www.pepper.pl/grupa/uslugi-i-subskrypcje?page={}',
+#         'udemy.com': 'https://www.pepper.pl/promocje/udemy.com?page={}'}
 
 
 # cookie = s.cookies.set("hide_expired=%221%22", "the cookie works",
@@ -17,14 +21,21 @@ def get(url):
     return r
 
 
-def post(url):
-    r = requests.post(url)
-    return r
+def session():
+    s = requests.Session()
+    s.headers.update({
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"})
+    return s
 
 
 def pages_count(response):
     soup = bs(response.text, 'html.parser')
-    count = int(soup.find('span', {'class': re.compile('last-page')}).text)
+
+    try:
+        count = int(soup.find('span', {'class': re.compile('last-page')}).text)
+    except AttributeError:
+        count = 1
+
     return count
 
 
@@ -36,27 +47,109 @@ def set_cookie():
     return cookie
 
 
-def scrape_pepper():
-    pages = []
+# def get_urls_data(s):
+#     global data, cookies
+#     for url in urls:
+#         data = s.get(urls[url])
+#         cookies = s.get(urls[url], cookies=set_cookie())
+#
+#     return data, cookies
 
-    s = requests.Session()
-    s.headers.update({
-                         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"})
-    s.get(url_pepper)
+# def uslugi():
+#     return
+#
+# def szkolenia():
+#     return
+#
+# def udemy():
+#     return
 
-    cookie = s.get(url_pepper, cookies=set_cookie())
 
-    # r = s.get(url_pepper.format(1))
-    for page in range(pages_count(cookie)):
+
+
+
+def scrape_pepper(uslugi_subskrypcje, szkolenia_kursy, udemy):
+
+    # /szkolenia-i-kursy
+    endpoint_1_data = []
+    # /uslugi-i-subskrypcje
+    endpoint_2_data = []
+    # /udemy.com
+    endpoint_3_data = []
+
+    urls = ['https://www.pepper.pl/grupa/szkolenia-i-kursy?page={}',
+            'https://www.pepper.pl/grupa/uslugi-i-subskrypcje?page={}',
+            'https://www.pepper.pl/promocje/udemy.com?page={}']
+
+    data = []
+
+
+    # set cookie to disable expired offers for every endpoint
+    expired_on_endpoint_1 = session().get(uslugi_subskrypcje, cookies=set_cookie())
+    expired_on_endpoint_2 = session().get(szkolenia_kursy, cookies=set_cookie())
+    expired_on_endpoint_3 = session().get(udemy, cookies=set_cookie())
+
+    # argspec = inspect.getfullargspec(scrape_pepper)
+
+
+
+    for page in range(pages_count(expired_on_endpoint_1)):
         page += 1
-        # url = url_pepper.format(page)
-        # resp = get(url)
 
-        filtered_page = s.get(url_pepper.format(page), cookies=set_cookie())
+        filtered_page = session().get(uslugi_subskrypcje.format(page), cookies=set_cookie())
 
-        pages.append(find_free_offers(filtered_page))
+        endpoint_1_data.append(find_free_offers(filtered_page))
 
-    return pages
+    for page in range(pages_count(expired_on_endpoint_2)):
+        page += 1
+
+        filtered_page = session().get(szkolenia_kursy.format(page), cookies=set_cookie())
+
+        endpoint_2_data.append(find_free_offers(filtered_page))
+
+    for page in range(pages_count(expired_on_endpoint_3)):
+        page += 1
+
+        filtered_page = session().get(udemy.format(page), cookies=set_cookie())
+
+        endpoint_3_data.append(find_free_offers(filtered_page))
+
+    return endpoint_1_data, endpoint_2_data, endpoint_3_data
+
+
+# for endpoints in range(len(urls)):
+    #
+    #     expired_on = session().get(urls[endpoints], cookies=set_cookie())
+    #
+    #     for page in range(pages_count(expired_on)):
+    #         print(page)
+    #
+    #         filtered_page = session().get(urls[endpoints].format(page + 1), cookies=set_cookie())
+    #
+    #         data.append(find_free_offers(filtered_page))
+    #
+    # return data
+
+
+# SCRAPE WHOLE SITE /promocje
+# def scrape_pepper(url):
+#
+#     data = []
+#
+#
+#     # set cookie to disable expired offers for every endpoint
+#
+#     ex = session().get(url, cookies=set_cookie())
+#
+#     for page in range(pages_count(ex)):
+#         page += 1
+#
+#         filtered_page = session().get(url.format(page), cookies=set_cookie())
+#
+#         data.append(find_free_offers(filtered_page))
+#         print(page)
+#
+#     return data
 
 
 def find_free_offers(response):
@@ -71,11 +164,10 @@ def find_free_offers(response):
         # price = article.find('span', {'class': re.compile('thread-price')})
         # title = article.find('a', {'class': re.compile('thread-title')})
         # re_href = ".*https://www.pepper.pl/promocje/.*"
-        title = article.find('a', href=re.compile(".*https://www.pepper.pl/promocje/.*"))
+        title = article.find('a', href=re.compile("web|development|kursy"))
 
         # TODO: mam linki, teraz zrobic jakies dictionary do tego, dodac cene opisy, link, wypisywac nie wszystkie promocje
         #   a tylko te za darmo i jedynie kursy a nie wszystkie oferty
-
 
         # offer_description = article.find('div', {'class': re.compile('title')}).strong.a
 
@@ -87,7 +179,6 @@ def find_free_offers(response):
             data.append(title['href'])
         except TypeError:
             pass
-
 
         # if offer_description is not None:
         #     data.append(offer_description.text)
